ec747cbca71245bfe2c3bb0efc5429370052df9f
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-04-15 16:14:58 +0100

    Updated install script to work on different Linux versions

--- amp-install.sh
+++ amp-install.sh
@@ -55,7 +55,7 @@ function error() {
 }
 
 function usage() {
-    echo "Usage: $(basename ${0}) [-h] [-q] [-e] [-s] [-u user] [-k key] hostname"
+    echo "Usage: $(basename ${0}) [-h] [-q] [-e] [-s] [-u user] [-k key] [-p port] hostname"
     exit 1
 }
 
@@ -63,7 +63,7 @@ QUIET=false
 LOG="amp-install.log"
 AMP_VERSION="2.0.0-M1"
 
-while getopts ":hesu:k:q" o; do
+while getopts ":hesu:k:q:p:" o; do
     case "${o}" in
         h)  help
             ;;
@@ -76,7 +76,9 @@ while getopts ":hesu:k:q" o; do
         k)  PRIVATE_KEY_FILE="${OPTARG}"
             ;;
         q)  QUIET=true
-            ;;
+            ;;    
+        p)  PORT="${OPTARG}"
+            ;;              
         *)  usage "Invalid option: $*"
             ;;
     esac
@@ -88,6 +90,7 @@ if [ $# -ne 1 ]; then
 fi
 
 HOST="${1}"
+PORT="${PORT:-22}"
 USER="${AMP_USER:-amp}"
 PRIVATE_KEY_FILE="${PRIVATE_KEY_FILE:-${HOME}/.ssh/id_rsa}"
 
@@ -97,66 +100,99 @@ if [ -f ${PRIVATE_KEY_FILE} ]; then
 fi
 SSH_PUBLIC_KEY_DATA=$(ssh-keygen -y -f ${PRIVATE_KEY_FILE})
 
-echo "Installing AMP ${AMP_VERSION} on ${HOST} as '${USER}'"
+echo "Installing AMP ${AMP_VERSION} on ${HOST} (using port $PORT) as '${USER}'"
+
+# pre-requisites for this script
+${QUIET} || echo "Installing pre-requisites..."
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "which which || { yum -y -q install which || apt-get -y install which; }" > ${LOG} 2>&1
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "which curl || { yum -y -q install curl || apt-get -y install curl; }" > ${LOG} 2>&1
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "which sed || { yum -y -q install sed || apt-get -y install sed; }" > ${LOG} 2>&1
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "which tar || { yum -y -q install tar || apt-get -y install tar; }" > ${LOG} 2>&1
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "which gunzip || { yum -y -q install gunzip || apt-get -y install gunzip; }" > ${LOG} 2>&1
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "which git || { yum -q -y install git || apt-get -y install git; }" > ${LOG} 2>&1
+# install java6
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "which java || { yum -y -q install java-1.6.0-openjdk || apt-get update && apt-get -y install openjdk-6-jre-headless; }" > ${LOG} 2>&1 
+if [ -r /etc/lsb-release ]; then
+    JAVA_HOME="/usr/lib/jvm/java-1.6.0-openjdk-amd64/"
+else
+    JAVA_HOME="/usr/lib/jvm/jre/"
+fi
+# install maven
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "mkdir -p /usr/local/apache-maven/"
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "curl -s -o /usr/local/apache-maven/apache-maven-3.2.1-bin.tar.gz https://s3-eu-west-1.amazonaws.com/cloudsoft-amp/apache-maven-3.2.1-bin.tar.gz"
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "tar xzf /usr/local/apache-maven/apache-maven-3.2.1-bin.tar.gz -C /usr/local/apache-maven/"
+# increase linux kernel entropy for faster ssh connections
+ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "which rng-tools || { yum -y -q install rng-tools || apt-get -y install rng-tools; }" > ${LOG} 2>&1
+if [ -f "/etc/default/rng-tools" ]; then
+    ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "cat >> /etc/default/rng-tools" <<EOF
+HRNGDEVICE=/dev/urandom
+EOF
+    ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "/etc/init.d/rng-tools start" > ${LOG} 2>&1
+else
+    ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "cat >> /etc/sysconfig/rngd" <<EOF
+EXTRAOPTIONS="-r /dev/urandom"
+EOF
+    ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "/etc/init.d/rngd start" > ${LOG} 2>&1
+fi
+
+${QUIET} || echo "...finished"
 
 # Create AMP user if required
-if ! ssh ${SSH_OPTS} root@${HOST} "id -o ${USER} > /dev/null 2>&1"; then
+if ! ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "id -o ${USER} > /dev/null 2>&1"; then
     if [ -z "${SETUP_USER}" ]; then
         error "User '${USER}' does not exist on ${HOST}"
     fi
     ${QUIET} || echo -n "Creating user '${USER}'..."
-    ssh ${SSH_OPTS} root@${HOST} "useradd ${USER}" > ${LOG} 2>&1
+    ssh ${SSH_OPTS} root@${HOST}  -p ${PORT} "useradd ${USER} -s '/bin/bash' -d \"/home/${USER}\" -m" > ${LOG} 2>&1
     ${QUIET} || echo "...finished"
 fi
 
 # Setup AMP user
 if [ "${SETUP_USER}" ]; then
     ${QUIET} || echo -n "Setting up user '${USER}'..."
-    ssh ${SSH_OPTS} root@${HOST} "echo '${USER} ALL = (ALL) NOPASSWD: ALL' >> /etc/sudoers"
-    ssh ${SSH_OPTS} root@${HOST} "sed -i.brooklyn.bak 's/.*requiretty.*/#brooklyn-removed-require-tty/' /etc/sudoers"
-    ssh ${SSH_OPTS} root@${HOST} "mkdir -p /home/${USER}/.ssh"
-    ssh ${SSH_OPTS} root@${HOST} "chmod 700 /home/${USER}/.ssh"
-    ssh ${SSH_OPTS} root@${HOST} "echo ${SSH_PUBLIC_KEY_DATA} >> /home/${USER}/.ssh/authorized_keys"
-    ssh ${SSH_OPTS} root@${HOST} "chown -R ${USER}.${USER} /home/${USER}/.ssh"
-    ssh ${SSH_OPTS} ${USER}@${HOST} "ssh-keygen -q -t rsa -N \"\" -f .ssh/id_rsa"
-    ssh ${SSH_OPTS} ${USER}@${HOST} "ssh-keygen -y -f .ssh/id_rsa | cat >> .ssh/authorized_keys"
+    ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "echo '${USER} ALL = (ALL) NOPASSWD: ALL' >> /etc/sudoers"
+    ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "sed -i.brooklyn.bak 's/.*requiretty.*/#brooklyn-removed-require-tty/' /etc/sudoers"
+    ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "mkdir -p /home/${USER}/.ssh"
+    ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "chmod 700 /home/${USER}/.ssh"
+    ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "echo ${SSH_PUBLIC_KEY_DATA} >> /home/${USER}/.ssh/authorized_keys"
+    ssh ${SSH_OPTS} root@${HOST} -p ${PORT} "chown -R ${USER}.${USER} /home/${USER}/.ssh"
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "ssh-keygen -q -t rsa -N \"\" -f .ssh/id_rsa"
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "ssh-keygen -y -f .ssh/id_rsa | cat >> .ssh/authorized_keys"
     ${QUIET} || echo "...finished"
 fi
 
 # Setup AMP
 ${QUIET} || echo -n "Installing AMP..."
-ssh ${SSH_OPTS} ${USER}@${HOST} "curl -s -o cloudsoft-amp-${AMP_VERSION}.tar.gz https://s3-eu-west-1.amazonaws.com/cloudsoft-amp/cloudsoft-amp-${AMP_VERSION}.tar.gz"
-ssh ${SSH_OPTS} ${USER}@${HOST} "tar zxf cloudsoft-amp-${AMP_VERSION}.tar.gz"
+ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "curl -s -o cloudsoft-amp-${AMP_VERSION}.tar.gz https://s3-eu-west-1.amazonaws.com/cloudsoft-amp/cloudsoft-amp-${AMP_VERSION}.tar.gz"
+ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "tar zxf cloudsoft-amp-${AMP_VERSION}.tar.gz"
 ${QUIET} || echo "...finished"
 
 # Configure AMP if no brooklyn.properties
-if ! ssh ${SSH_OPTS} ${USER}@${HOST} "test -f .brooklyn/brooklyn.properties"; then
+if ! ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "test -f .brooklyn/brooklyn.properties"; then
     ${QUIET} || echo -n "Configuring AMP..."
-    ssh ${SSH_OPTS} ${USER}@${HOST} "mkdir -p .brooklyn"
-    ssh ${SSH_OPTS} ${USER}@${HOST} "curl -s -o .brooklyn/brooklyn.properties http://brooklyncentral.github.io/use/guide/quickstart/brooklyn.properties"
-    ssh ${SSH_OPTS} ${USER}@${HOST} "sed -i.bak 's/^# brooklyn.webconsole.security.provider = brooklyn.rest.security.provider.AnyoneSecurityProvider/brooklyn.webconsole.security.provider = brooklyn.rest.security.provider.AnyoneSecurityProvider/' .brooklyn/brooklyn.properties"
-    ssh ${SSH_OPTS} ${USER}@${HOST} "curl -s -o .brooklyn/catalog.xml http://brooklyncentral.github.io/use/guide/quickstart/catalog.xml"
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "mkdir -p .brooklyn"
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "curl -s -o .brooklyn/brooklyn.properties http://brooklyncentral.github.io/use/guide/quickstart/brooklyn.properties"
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "sed -i.bak 's/^# brooklyn.webconsole.security.provider = brooklyn.rest.security.provider.AnyoneSecurityProvider/brooklyn.webconsole.security.provider = brooklyn.rest.security.provider.AnyoneSecurityProvider/' .brooklyn/brooklyn.properties"
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "curl -s -o .brooklyn/catalog.xml http://brooklyncentral.github.io/use/guide/quickstart/catalog.xml"
     ${QUIET} || echo "...finished"
 fi
 
 # Install example Jars and catalog
 if [ "${INSTALL_EXAMPLES}" ]; then
     ${QUIET} || echo -n "Installing examples..."
-    # Install required development tools
-    ssh ${SSH_OPTS} root@${HOST} "curl -s -o /etc/yum.repos.d/epel-apache-maven.repo http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo"
-    ssh ${SSH_OPTS} root@${HOST} "yum -q -y install apache-maven" > ${LOG} 2>&1
-    ssh ${SSH_OPTS} root@${HOST} "yum -q -y install git" > ${LOG} 2>&1
-    ssh ${SSH_OPTS} ${USER}@${HOST} "cat >> ~/.bashrc" <<EOF
-export M2_HOME=/usr/share/apache-maven
-export PATH=\${M}2_HOME/bin:\${PATH}
+    # Configure required development tools
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "echo 'JAVA_HOME=${JAVA_HOME}' >> ~/.bashrc"
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "cat >> ~/.bashrc" <<EOF
+export M2_HOME=/usr/local/apache-maven/apache-maven-3.2.1
+export PATH=\${M2_HOME}/bin:\${PATH}
 EOF
     ${QUIET} || echo -n "..."
 
     # Build and install OpenGamma blueprints
-    ssh ${SSH_OPTS} ${USER}@${HOST} "mkdir projects"
-    ssh ${SSH_OPTS} ${USER}@${HOST} "git clone https://github.com/cloudsoft/brooklyn-opengamma.git projects/brooklyn-opengamma" > ${LOG} 2>&1
-    ssh ${SSH_OPTS} ${USER}@${HOST} "mvn --quiet clean install -f ./projects/brooklyn-opengamma/pom.xml" > ${LOG} 2>&1
-    ssh ${SSH_OPTS} ${USER}@${HOST} "cat > .brooklyn/catalog.xml" <<EOF
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "mkdir projects"
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "git clone https://github.com/cloudsoft/brooklyn-opengamma.git projects/brooklyn-opengamma" > ${LOG} 2>&1
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "bash -ic 'mvn --quiet clean install -f ./projects/brooklyn-opengamma/pom.xml'" > ${LOG} 2>&1
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "cat > .brooklyn/catalog.xml" <<EOF
 <?xml version="1.0"?>
 <catalog>
   <name>AMP Example Blueprints</name>
@@ -179,13 +215,13 @@ EOF
   </classpath>
 </catalog>
 EOF
-    ssh ${SSH_OPTS} ${USER}@${HOST} "curl -s -o .brooklyn/MaxMind-GeoLiteCity.dat.gz http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz"
-    ssh ${SSH_OPTS} ${USER}@${HOST} "gunzip .brooklyn/MaxMind-GeoLiteCity.dat.gz"
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "curl -s -o .brooklyn/MaxMind-GeoLiteCity.dat.gz http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz"
+    ssh ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "gunzip .brooklyn/MaxMind-GeoLiteCity.dat.gz"
     ${QUIET} || echo "...finished"
 fi
 
 # Run AMP
 ${QUIET} || echo -n "Starting AMP..."
-ssh -n -f ${SSH_OPTS} ${USER}@${HOST} "nohup ./cloudsoft-amp-${AMP_VERSION}/bin/amp launch >> ./cloudsoft-amp-${AMP_VERSION}/amp-console.log 2>&1 &"
+ssh -n -f ${SSH_OPTS} ${USER}@${HOST} -p ${PORT} "nohup ./cloudsoft-amp-${AMP_VERSION}/bin/amp launch >> ./cloudsoft-amp-${AMP_VERSION}/amp-console.log 2>&1 &"
 ${QUIET} || echo "...finished"
 echo "Console URL is http://${HOST}:8081/"
